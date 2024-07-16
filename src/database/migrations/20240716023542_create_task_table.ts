5c88dc01945dbb96b83166f3c86625dbe36161be
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-07-16 08:25:07 +0545

    add migrations

---
+++ src/database/migrations/20240716023542_create_task_table.ts
@@ -0,0 +1,58 @@
+import { Knex } from 'knex';
+
+const TABLE_NAME = 'tasks';
+
+
+/**
+ * Create table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(TABLE_NAME, (table) => {
+    table.bigIncrements();
+    table.string("name").notNullable();
+
+    table
+      .integer("user_id")
+      .unsigned()
+      .nullable()
+      .references("id")
+      .inTable("users");
+    table
+      .integer("status_id")
+      .unsigned()
+      .nullable()
+      .references("id")
+      .inTable("status");
+    
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+    
+    table
+      .bigInteger('created_by')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable("users");
+      
+    table.timestamp('updated_at').nullable();
+    
+    table
+      .bigInteger('updated_by')
+      .unsigned()
+      .references('id')
+      .inTable("users")
+      .nullable();
+  });
+}
+
+/**
+ * Drop table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable(TABLE_NAME);
+}
