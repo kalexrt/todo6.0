1d44ce5fa9a188392bc6ea9aca39c2c54afc983c
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-07-16 08:36:01 +0545

    add seeds

---
+++ src/database/seeds/user_permissions.ts
@@ -0,0 +1,30 @@
+import { Knex } from 'knex';
+
+const TABLE_NAME = 'user_permissions';
+
+/**
+ * Delete existing entries and seed values for table TABLE_NAME.
+ *
+ * @param   {Knex} knex
+ * @returns {Promise}
+ */
+export function seed(knex: Knex): Promise<void> {
+  return knex(TABLE_NAME)
+    .del()
+    .then(() => {
+      return knex(TABLE_NAME).insert([
+        {
+          permission_id: 1,
+          user_id: 1,
+        },
+        {
+          permission_id: 2,
+          user_id: 1,
+        },
+        {
+          permission_id: 3,
+          user_id: 1,
+        },
+      ]);
+    });
+}
